4f58ed1da5db9232f9179a06db5d75d73c2c2fa0
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-09-30 16:48:06 +0100

    Add stack form component

---
+++ src/components/StackForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Form, FormGroup, FormControl, ControlLabel, Button } from 'react-bootstrap';
+
+class StackForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      title: '',
+      cards: []
+    }
+  }
+
+  addCard() {
+    const  { cards } = this.state;
+
+    cards.push({ id: cards.length, prompt: '', answer: ''});
+
+    this.setState({ cards });
+  }
+
+
+  render() {
+    return (
+      <div>
+        <Link to='/'className='homelink'>
+          <h4>Home</h4>
+        </Link>
+        <h4>Create a new Stack</h4>
+        <br />
+        <Form inline>
+          <FormGroup>
+            <ControlLabel>Title:</ControlLabel>
+            {' '}
+            <FormControl />
+          </FormGroup>
+        </Form>
+        <br />
+        <Button onClick={() => this.addCard()}>Add Card</Button>
+      </div>
+    )
+  }
+}
+
+export default StackForm;
