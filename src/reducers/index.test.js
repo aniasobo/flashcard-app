5d9283e7121e9782614d5221f886e1ce050b3b7d
Anna Sobolewska <sobolewska.a.m@gmail.com>
2019-10-10 16:55:58 +0100

    Complete test coverage for reducers

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -8,5 +8,18 @@ describe('root reducer', () => {
   it('returns the initial state by default', () => {
     expect(rootReducer({}, {})).toEqual(defaultObject);
   });
+
+  it('sets the main stack', () => {
+    expect(rootReducer({}, { type: actions.SET_STACK, stack })).toEqual('foo');
+  });
+
+  it('loads stacks', () => {
+    expect(rootReducer({}, { type: actions.LOAD_STACKS, stacks })).toEqual({stack: {}, stacks});
+  });
+
+  it('adds a stack', () => {
+    const testStack = { id: 3, title: 'stackity stack', cards: [] };
+    expect(rootReducer({}, { type: actions.ADD_STACK, testStack })).toEqual({stack: {}, stacks: [{...testStack, id: 0}]});
+  });
 });
 
